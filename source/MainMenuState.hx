95ab7264cabf224ffaf45dc87ddb722bf8ec8e8a
JustinSolstice <89734833+JustinSolstice@users.noreply.github.com>
2022-01-02 15:23:00 -0500

    Update MainMenuState.hx

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -132,7 +132,7 @@ class MainMenuState extends MusicBeatState
 
 		FlxG.camera.follow(camFollowPos, null, 1);
 
-		var versionShit:FlxText = new FlxText(12, FlxG.height - 44, 0, "Psych Engine v" + psychEngineVersion + " Moddified by JustinSolstice", 12);
+		var versionShit:FlxText = new FlxText(12, FlxG.height - 44, 0, "Custom Build based on Psych Engine v" + psychEngineVersion, 12);
 		versionShit.scrollFactor.set();
 		versionShit.setFormat("VCR OSD Mono", 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 		add(versionShit);
